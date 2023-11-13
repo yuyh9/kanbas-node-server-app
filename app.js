873adc24e57bb7b6dd4245dce74835d281a2d320
HUIYING YU <87959635+yuyh9@users.noreply.github.com>
2023-11-12 19:54:49 -0800

    commit

---
+++ app.js
@@ -0,0 +1,20 @@
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import Hello from "./hello.js";
+import Lab5 from "./lab5.js";
+import CourseRoutes from "./courses/routes.js";
+import ModuleRoutes from "./modules/routes.js";
+const app = express();
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL
+  })
+);
+app.use(express.json());
+ModuleRoutes(app);
+CourseRoutes(app)
+Lab5(app);
+Hello(app);
+app.listen(process.env.PORT || 4000);
