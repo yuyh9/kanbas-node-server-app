a4bf8eee1df83363d043ab168e56a599421d5d0f
HUIYING YU <87959635+yuyh9@users.noreply.github.com>
2023-11-13 23:04:26 -0800

    commit

--- courses/routes.js
+++ courses/routes.js
@@ -1,14 +1,22 @@
 import Database from "../Database/index.js";
 function CourseRoutes(app) {
-  app.get("/api/courses/:id", (req, res) => {
-    const { id } = req.params;
-    const course = Database.courses
-      .find((c) => c._id === id);
-    if (!course) {
-      res.status(404).send("Course not found");
-      return;
-    }
+  app.get("/api/courses", (req, res) => {
+    const courses = Database.courses;
+    res.send(courses);
+  });
+
+  app.post("/api/courses", (req, res) => {
+    const course = { ...req.body,
+      _id: new Date().getTime().toString() };
+    Database.courses.unshift(course);
     res.send(course);
+  });
+
+  app.delete("/api/courses/:id", (req, res) => {
+    const { id } = req.params;
+    Database.courses = Database.courses
+      .filter((c) => c._id !== id);
+    res.sendStatus(204);
   });
 
   app.put("/api/courses/:id", (req, res) => {
@@ -19,24 +27,16 @@ function CourseRoutes(app) {
     );
     res.sendStatus(204);
   });
-
-  app.delete("/api/courses/:id", (req, res) => {
+  
+  app.get("/api/courses/:id", (req, res) => {
     const { id } = req.params;
-    Database.courses = Database.courses
-      .filter((c) => c._id !== id);
-    res.sendStatus(204);
-  });
-
-  app.post("/api/courses", (req, res) => {
-    const course = { ...req.body,
-      _id: new Date().getTime().toString() };
-    Database.courses.push(course);
+    const course = Database.courses
+      .find((c) => c._id === id);
+    if (!course) {
+      res.status(404).send("Course not found");
+      return;
+    }
     res.send(course);
-  });
-
-  app.get("/api/courses", (req, res) => {
-    const courses = Database.courses;
-    res.send(courses);
   });
 }
 
