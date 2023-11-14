b956fd6b46c97118680d3f7abc9d31253136be26
HUIYING YU <87959635+yuyh9@users.noreply.github.com>
2023-11-14 01:59:51 -0800

    commit

--- assignment/routes.js
+++ assignment/routes.js
@@ -1,35 +1,35 @@
 import db from "../Database/index.js";
 function AssignmentRoutes(app) {
-  app.get("/api/courses/:aid/assignments", (req, res) => {
-    const { aid } = req.params;
-    const assignments = db.modules
-      .filter((m) => m.course === aid);
+  app.get("/api/courses/:cid/assignments", (req, res) => {
+    const { cid } = req.params;
+    const assignments = db.assignments
+      .filter((a) => a.course === cid);
     res.send(assignments);
   });
 
-  app.post("/api/courses/:aid/assignments", (req, res) => {
-    const { aid } = req.params;
+  app.post("/api/courses/:cid/assignments", (req, res) => {
+    const { cid } = req.params;
     const newAssignment = {
       ...req.body,
-      course: aid,
+      course: cid,
       _id: new Date().getTime().toString(),
     };
-    db.modules.push(newAssignment);
+    db.assignments.push(newAssignment);
     res.send(newAssignment);
   });
 
   app.delete("/api/assignments/:aid", (req, res) => {
     const { aid } = req.params;
-    db.modules = db.modules.filter((m) => m._id !== aid);
+    db.assignments = db.assignments.filter((m) => m._id !== aid);
     res.sendStatus(200);
   });
 
   app.put("/api/assignments/:aid", (req, res) => {
     const { aid } = req.params;
-    const assignmentIndex = db.modules.findIndex(
-      (m) => m._id === mid);
-    db.modules[assignmentIndex] = {
-      ...db.modules[assignmentIndex],
+    const assignmentIndex = db.assignments.findIndex(
+      (m) => m._id === aid);
+    db.assignments[assignmentIndex] = {
+      ...db.assignments[assignmentIndex],
       ...req.body
     };
     res.sendStatus(204);
