f13beb39a2cba5930ffac622347cca6e9b8fc035
HUIYING YU <87959635+yuyh9@users.noreply.github.com>
2023-11-13 22:53:04 -0800

    commit

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: process.env.NODE_ENV === "production" ? process.env.FRONTEND_URL : process.env.FRONTEND_URL_LOCAL,
+    origin: process.env.FRONTEND_URL
   }));
 app.use(express.json());
 ModuleRoutes(app);
--- modules/routes.js
+++ modules/routes.js
@@ -1,20 +1,10 @@
 import db from "../Database/index.js";
 function ModuleRoutes(app) {
-  app.put("/api/modules/:mid", (req, res) => {
-    const { mid } = req.params;
-    const moduleIndex = db.modules.findIndex(
-      (m) => m._id === mid);
-    db.modules[moduleIndex] = {
-      ...db.modules[moduleIndex],
-      ...req.body
-    };
-    res.sendStatus(204);
-  });
-
-  app.delete("/api/modules/:mid", (req, res) => {
-    const { mid } = req.params;
-    db.modules = db.modules.filter((m) => m._id !== mid);
-    res.sendStatus(200);
+  app.get("/api/courses/:cid/modules", (req, res) => {
+    const { cid } = req.params;
+    const modules = db.modules
+      .filter((m) => m.course === cid);
+    res.send(modules);
   });
 
   app.post("/api/courses/:cid/modules", (req, res) => {
@@ -28,11 +18,21 @@ function ModuleRoutes(app) {
     res.send(newModule);
   });
 
-  app.get("/api/courses/:cid/modules", (req, res) => {
-    const { cid } = req.params;
-    const modules = db.modules
-      .filter((m) => m.course === cid);
-    res.send(modules);
+  app.delete("/api/modules/:mid", (req, res) => {
+    const { mid } = req.params;
+    db.modules = db.modules.filter((m) => m._id !== mid);
+    res.sendStatus(200);
+  });
+
+  app.put("/api/modules/:mid", (req, res) => {
+    const { mid } = req.params;
+    const moduleIndex = db.modules.findIndex(
+      (m) => m._id === mid);
+    db.modules[moduleIndex] = {
+      ...db.modules[moduleIndex],
+      ...req.body
+    };
+    res.sendStatus(204);
   });
 }
 export default ModuleRoutes;
