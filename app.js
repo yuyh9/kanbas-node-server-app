4a3605183ddcce403ce1e75d8fa8e9cbd16f1068
HUIYING YU <87959635+yuyh9@users.noreply.github.com>
2023-12-01 22:09:29 -0800

    commit

--- app.js
+++ app.js
@@ -1,21 +1,42 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
 import Hello from "./hello.js";
 import Lab5 from "./lab5.js";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 import AssignmentRoutes from "./assignment/routes.js";
+import UserRoutes from "./users/routes.js";
+import session from "express-session";
+const CONNECTION_STRING = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/kanbas';
+mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(
   cors({
     credentials: true,
     origin: process.env.NODE_ENV === "production" ? process.env.FRONTEND_URL : process.env.FRONTEND_URL_LOCAL,
-  }));
+  })
+);
+const sessionOptions = {
+  secret: "any string",
+  resave: false,
+  saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+  };
+}
+app.use(session(sessionOptions));
+
 app.use(express.json());
+UserRoutes(app);
 AssignmentRoutes(app);
 ModuleRoutes(app);
-CourseRoutes(app)
+CourseRoutes(app);
 Lab5(app);
 Hello(app);
 app.listen(process.env.PORT || 4000);
