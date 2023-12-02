d4fedc569be4fd6274a30f6c7b1b41f8dbf7a63b
HUIYING YU <87959635+yuyh9@users.noreply.github.com>
2023-12-02 02:56:22 -0800

    Update model.js

--- users/model.js
+++ users/model.js
@@ -1,4 +1,4 @@
 import mongoose from "mongoose";
 import schema from "./schema.js";
-const model = mongoose.model("users", schema, 'kanbas');
+const model = mongoose.model("users", schema);
 export default model;
